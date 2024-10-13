03a8b057b0e15a9213008dcd4dbddcfd57fb0fb4
Schindler Thuner <thuner.schin@gmail.como>
2024-10-13 15:07:24 -0300

    criação da model proposal, e inicio da criação das views do projeto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,8 +18,13 @@ class DatabaseSeeder extends Seeder
         User::factory()->count(200)->create();
         //Fazendo uma consulta no banco de dados, embaralhando a ordem,
         //Retornando apenas 10, desses 10 pra cada 1 é criado um projeto
-        User::query()->inRandomOrder()->limit(10)->get()
-        ->each(fn (User $u) => Project::factory()->create(['created_by' => $u->id]));
+        // User::query()->inRandomOrder()->limit(10)->get()
+        // ->each(fn (User $u) => Project::factory()->create(['created_by' => $u->id]));
         // Project::factory()->count(10)->create();
+        User::query()->inRandomOrder()->limit(10)->get()
+        ->each(function (User $u){
+            $project = Project::factory()->create(['created_by' => $u->id]);
+            Proposal::factory()->count(random_int(4,45))->create(['project_id' => $project->id]);
+        });
     }
 }
